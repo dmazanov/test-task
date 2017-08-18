0a08aac934e9496851a1f42833bd6198ddb11425
Denis <dmazanov@mail.ru>
2017-08-18 18:13:33 +0200

    add browserify and install dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var gulp = require('gulp'),
 		rename = require("gulp-rename"),
 		sourcemaps = require('gulp-sourcemaps'),
 		autoprefixer = require('gulp-autoprefixer'),
+		browserify = require('browserify'),
+		source = require('vinyl-source-stream'),
+		buffer = require('vinyl-buffer'),
+		uglify = require('gulp-uglify'),
+		gutil = require('gulp-util'),
 		browserSync = require('browser-sync').create(),
 		reload = browserSync.reload;
 
@@ -26,8 +31,27 @@ gulp.task('sass', function() {
 });
 
 
+gulp.task('javascript', function() {
+		var b = browserify({
+			entries: './src/js/main.js',
+			debug: true
+		});
+
+		return b.bundle()
+		.pipe(source('./build.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({loadMaps: true}))
+				// Add transformation tasks to the pipeline here
+				.pipe(uglify())
+				.on('error', gutil.log)
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest('./js/'))
+		.pipe(browserSync.stream())
+});
+
+
 // Static Server + watching scss/js/html files
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['sass', 'javascript'], function() {
 	browserSync.init({
 		server: {
 			baseDir: './'
@@ -35,7 +59,7 @@ gulp.task('watch', ['sass'], function() {
 	});
 	gulp.watch("*.html").on('change', browserSync.reload);
 	gulp.watch("./src/scss/**/*.scss", ['sass']);
-	gulp.watch("./src/js/**/*.js").on('change', browserSync.reload);
+	gulp.watch("./src/js/**/*.js", ['javascript']);
 });
 
 gulp.task('default', ['watch']);
